60bf1982c1a537290bc085a49418958f3f763267
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-02 10:16:29 +0100

    First definition of a generation as a set of coordinates representing the location of "living" cells

--- src/life/Life.scala
+++ src/life/Life.scala
@@ -1,9 +1,49 @@
 package life
+import scala.collection.mutable.HashSet
 
 object Life {
 
   def main(args: Array[String]): Unit = {
-    println("Hello")
+    val initialCells = new HashSet[Coordinate]
+    initialCells.add(new Coordinate(5, 5))
+    val g0 = new Generation(10, 10, initialCells)
+    println(g0.toString())
   }
 
+}
+
+class Coordinate(val x: Int, val y: Int) {
+  override def toString = {"(" + x + ", " + y + ")"}
+  override def hashCode = 11 + x*101 + y;
+  override def equals(other: Any) = {
+    other match {
+      case that: Coordinate => this.x == that.x && this.y == that.y
+      case _ => false
+    }
+  }
+}
+
+
+class Generation(width: Int, height: Int, cells: HashSet[Coordinate]) {
+  
+  override def toString = {
+    var output: String = ""
+    for (x <- 0 to width;
+         y <- 0 to height;
+         coord = new Coordinate(x, y)) {
+      if (cells.contains(coord)) {
+        output += "*" 
+      } else {
+        output += "-"
+      }
+      if (y == width) {
+        output += "\n"
+      }
+    }
+    output;
+  }
+  
+  def nextGeneration(): Generation = {
+    new Generation(width, height, cells)
+  }
 }
