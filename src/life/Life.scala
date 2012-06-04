b45471681bf33fd1d157b6141bad3309e4ffb3b4
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-04 12:28:07 +0100

    Coordinates now can tell you who their neighbours are

--- src/life/Life.scala
+++ src/life/Life.scala
@@ -8,6 +8,9 @@ object Life {
     initialCells.add(new Coordinate(5, 5))
     val g0 = new Generation(10, 10, initialCells)
     println(g0.toString())
+    
+    println(new Coordinate(0,0).getNeighbours(5, 5))
+    println(new Coordinate(2,2).getNeighbours(5, 5))
   }
 
 }
@@ -21,22 +24,29 @@ class Coordinate(val x: Int, val y: Int) {
       case _ => false
     }
   }
+  
+  def getNeighbours(width:Int, height:Int) = {
+    for (xOffset <- -1 to 1;
+        yOffset <- -1 to 1 )
+    yield new Coordinate((x + xOffset + width) % width, (y + yOffset + width) % height)
+  }
 }
+
 
 
 class Generation(width: Int, height: Int, cells: HashSet[Coordinate]) {
   
   override def toString = {
     var output: String = ""
-    for (x <- 0 to width;
-         y <- 0 to height;
+    for (x <- 0 to width - 1;
+         y <- 0 to height - 1;
          coord = new Coordinate(x, y)) {
       if (cells.contains(coord)) {
         output += "*" 
       } else {
         output += "-"
       }
-      if (y == width) {
+      if (y == width - 1) {
         output += "\n"
       }
     }
