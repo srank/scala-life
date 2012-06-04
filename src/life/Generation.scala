579bcd446f4a20f7540b90d3d3b24215ad7f6e7e
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-04 13:52:56 +0100

    Generation.toString Now shows the board the right way round

--- src/life/Generation.scala
+++ src/life/Generation.scala
@@ -5,15 +5,15 @@ class Generation(width: Int, height: Int, cells: HashSet[Coordinate]) {
   
   override def toString = {
     var output: String = ""
-    for (x <- 0 to width - 1;
-         y <- 0 to height - 1;
+    for (y <- 0 to height - 1;
+         x <- 0 to width - 1;
          coord = new Coordinate(x, y)) {
       if (cells.contains(coord)) {
         output += "*" 
       } else {
         output += "-"
       }
-      if (y == width - 1) {
+      if (x >= width - 1) {
         output += "\n"
       }
     }
--- src/life/Life.scala
+++ src/life/Life.scala
@@ -5,12 +5,13 @@ object Life {
 
   def main(args: Array[String]): Unit = {
     val initialCells = new HashSet[Coordinate]
+    initialCells.add(new Coordinate(4, 5))
     initialCells.add(new Coordinate(5, 5))
+    initialCells.add(new Coordinate(6, 5))
     val g0 = new Generation(10, 10, initialCells)
     println(g0.toString())
     
     println(new Coordinate(0,0).getNeighbours(5, 5))
     println(new Coordinate(2,2).getNeighbours(5, 5))
   }
-
 }
