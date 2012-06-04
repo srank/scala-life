6bfae5570fbf02464a81653ba2a53c8c2d7c97da
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-04 16:27:10 +0100

    Todos and a bit more in the example

--- src/life/Life.scala
+++ src/life/Life.scala
@@ -9,7 +9,13 @@ object Life {
     initialCells.add(new Coordinate(4, 5))
     initialCells.add(new Coordinate(5, 5))
     initialCells.add(new Coordinate(6, 5))
+    
+    initialCells.add(new Coordinate(8, 0))
+    initialCells.add(new Coordinate(9, 0))
+    initialCells.add(new Coordinate(0, 0))
+    
     val g0 = new Generation(10, 10, initialCells)
+    println("Generation 0")
     println(g0.toString())
     
     println(new Coordinate(0,0).getNeighbours(5, 5))
@@ -18,7 +24,9 @@ object Life {
     println(g0.cellsToConsider);
     println(g0.nextGenerationsLivingCells)
     
+    println("Generation 1")
     println(g0.nextGeneration)
+    println("Generation 2")
     println(g0.nextGeneration.nextGeneration)
   }
 }
