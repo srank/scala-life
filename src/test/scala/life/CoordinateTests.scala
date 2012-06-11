727f48f3d6007d1f1ad301e5e7b1bce42b1f60e5
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-11 19:55:08 +0100

    Another test

--- src/test/scala/life/CoordinateTests.scala
+++ src/test/scala/life/CoordinateTests.scala
@@ -2,11 +2,24 @@ package life
 
 import org.scalatest.Suite
 
-
 class CoordinateTests extends Suite {
-  def testCoordinateNeighbours {
-    val coordinate = new Coordinate(5,3)
+  val coordinate = new Coordinate(3, 5)
+
+  def testCoordsAreDecomposable() {
+    assert(coordinate.x == 3)
+    assert(coordinate.y == 5)
+  }
+  
+  def testSimpleCoordinateNeighbours() {
+    val neighbours = coordinate.getNeighbours(10,10)
     
-    assert(coordinate.x == 5)
+    assert(neighbours.length == 8)
+    
+    val expectedNeighbours = Array((2, 4), (2, 5), (2, 6), (3, 4), (3, 6), (4, 4), (4, 5), (4, 6))
+    
+    for ((x, y) <- expectedNeighbours;
+         n = new Coordinate(x, y)) {
+      assert(neighbours.contains(n) )
+    }
   }
 }
