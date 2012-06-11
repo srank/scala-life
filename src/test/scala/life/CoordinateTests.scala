1447b50ba62da9c57f1bc8c97f5cf7add860eeb5
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-11 19:58:28 +0100

    Testing wrapping in calculating neighbours

--- src/test/scala/life/CoordinateTests.scala
+++ src/test/scala/life/CoordinateTests.scala
@@ -5,12 +5,12 @@ import org.scalatest.Suite
 class CoordinateTests extends Suite {
   val coordinate = new Coordinate(3, 5)
 
-  def testCoordsAreDecomposable() {
+  def testCoordsAreDecomposable {
     assert(coordinate.x == 3)
     assert(coordinate.y == 5)
   }
   
-  def testSimpleCoordinateNeighbours() {
+  def testSimpleCoordinateNeighbours {
     val neighbours = coordinate.getNeighbours(10,10)
     
     assert(neighbours.length == 8)
@@ -19,7 +19,21 @@ class CoordinateTests extends Suite {
     
     for ((x, y) <- expectedNeighbours;
          n = new Coordinate(x, y)) {
-      assert(neighbours.contains(n) )
+      assert(neighbours.contains(n), "Coordinate " + n + "not found in " + neighbours)
+    }
+    
+  }
+  
+  def testWrappingCoordinateNeighbours {
+    val neighbours = coordinate.getNeighbours(3,5)
+    
+    assert(neighbours.length == 8)
+    
+    val expectedNeighbours = Array((2, 2), (2, 3), (2, 4), (0, 2), (0, 4), (1, 2), (1, 3), (1, 4))
+    
+    for ((x, y) <- expectedNeighbours;
+         n = new Coordinate(x, y)) {
+      assert(neighbours.contains(n), "Coordinate " + n + "not found in " + neighbours)
     }
   }
 }
