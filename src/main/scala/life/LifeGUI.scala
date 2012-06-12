f2fecf3a7b7d2dcba44b5e07c5caefaa37c80229
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-12 22:20:23 +0100

    Uncommenting code - I think this was commented out while I was fiddling with project and classpath settings

--- src/main/scala/life/LifeGUI.scala
+++ src/main/scala/life/LifeGUI.scala
@@ -1,12 +1,12 @@
-//package life
-//
-//import scala.swing._
-//
-//object FirstSwingApp extends SimpleGUIApplication {
-//  def top = new MainFrame {
-//    title = "Scala life"
-//        contents = new Button {
-//      text = "Click me"
-//    }
-//  }
-//}
+package life
+
+import scala.swing._
+
+object FirstSwingApp extends SimpleGUIApplication {
+  def top = new MainFrame {
+    title = "Scala life"
+        contents = new Button {
+      text = "Click me"
+    }
+  }
+}
