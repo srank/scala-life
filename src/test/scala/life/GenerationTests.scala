96392105dc7f36e4b62de2142ba0a0ee625ad060
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-12 22:03:36 +0100

    Even more complex test - checking overlap

--- src/test/scala/life/GenerationTests.scala
+++ src/test/scala/life/GenerationTests.scala
@@ -27,8 +27,38 @@ class GenerationTests extends FunSuite {
                             new Coordinate(0,7), new Coordinate(0,0), new Coordinate(0,1), 
                             new Coordinate(1,7), new Coordinate(1,0), new Coordinate(1,1))
     assert(expectedCells === cellsToConsider)
-
-
+  }
+  
+  test("cellsToConsider gets all neighbours of all living cells") {
+    val cells = new HashSet[Coordinate] with Set[Coordinate]
+    cells.add(new Coordinate(2,2))
+    cells.add(new Coordinate(5,5))
+    val generation = new Generation(8, 8, cells)
+    val cellsToConsider = generation.cellsToConsider
+    assert(cellsToConsider.size === 18)
+    val expectedCells = Set(new Coordinate(1,1), new Coordinate(1,2), new Coordinate(1,3),
+                            new Coordinate(2,1), new Coordinate(2,2), new Coordinate(2,3), 
+                            new Coordinate(3,1), new Coordinate(3,2), new Coordinate(3,3),
+                            new Coordinate(4,4), new Coordinate(4,5), new Coordinate(4,6),
+                            new Coordinate(5,4), new Coordinate(5,5), new Coordinate(5,6), 
+                            new Coordinate(6,4), new Coordinate(6,5), new Coordinate(6,6))
+    assert(expectedCells === cellsToConsider)
+  }
+  
+    test("cellsToConsider gets all neighbours of all living cells and handles overlap") {
+    val cells = new HashSet[Coordinate] with Set[Coordinate]
+    cells.add(new Coordinate(2,2))
+    cells.add(new Coordinate(3,3))
+    val generation = new Generation(8, 8, cells)
+    val cellsToConsider = generation.cellsToConsider
+    assert(cellsToConsider.size === 14)
+    val expectedCells = Set(new Coordinate(1,1), new Coordinate(1,2), new Coordinate(1,3),
+                            new Coordinate(2,1), new Coordinate(2,2), new Coordinate(2,3), 
+                            new Coordinate(3,1), new Coordinate(3,2), new Coordinate(3,3),
+                            /*new Coordinate(2,2), new Coordinate(2,3),*/ new Coordinate(2,4),
+                            /*new Coordinate(3,2), new Coordinate(3,3),*/ new Coordinate(3,4), 
+                            new Coordinate(4,2), new Coordinate(4,3), new Coordinate(4,4))
+    assert(expectedCells === cellsToConsider)
   }
   
   test("Grid with two blinkers has period two") {
