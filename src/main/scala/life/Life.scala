8e8159ff386cdc7125a8c8f0d671f619ae4f63f1
Stephen Rank <Stephen.Rank@gmail.com>
2012-06-14 21:33:42 +0100

    Use of the companion object

--- src/main/scala/life/Life.scala
+++ src/main/scala/life/Life.scala
@@ -6,20 +6,20 @@ object Life {
 
   def main(args: Array[String]): Unit = {
     val initialCells = Set[Coordinate]()
-    initialCells.add(new Coordinate(4, 5))
-    initialCells.add(new Coordinate(5, 5))
-    initialCells.add(new Coordinate(6, 5))
+    initialCells.add(Coordinate(4, 5))
+    initialCells.add(Coordinate(5, 5))
+    initialCells.add(Coordinate(6, 5))
     
-    initialCells.add(new Coordinate(8, 0))
-    initialCells.add(new Coordinate(9, 0))
-    initialCells.add(new Coordinate(0, 0))
+    initialCells.add(Coordinate(8, 0))
+    initialCells.add(Coordinate(9, 0))
+    initialCells.add(Coordinate(0, 0))
     
     val g0 = new Generation(10, 10, initialCells)
     println("Generation 0")
     println(g0.toString())
     
-    println(new Coordinate(0,0).getNeighbours(5, 5))
-    println(new Coordinate(2,2).getNeighbours(5, 5))
+    println(Coordinate(0,0).getNeighbours(5, 5))
+    println(Coordinate(2,2).getNeighbours(5, 5))
     
     println(g0.cellsToConsider);
     println(g0.nextGenerationsLivingCells)
