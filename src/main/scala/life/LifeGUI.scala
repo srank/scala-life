c1d1850f96f401f02f95005dbc2c19fa42a75129
Stephen Rank <Stephen.Rank@gmail.com>
2012-07-03 21:55:40 +0100

    GUI can now be persuaded to increment the generation

--- src/main/scala/life/LifeGUI.scala
+++ src/main/scala/life/LifeGUI.scala
@@ -1,24 +1,38 @@
 package life
 
 import scala.swing._
+import scala.swing.event.ButtonClicked
 
 object FirstSwingApp extends SimpleSwingApplication {
   def top = new MainFrame {
     val cells = Set(Coordinate(2,2), Coordinate(2,3), Coordinate(2,4))
-        val g = new Generation(10, 10, cells)
+    val g = new Generation(10, 10, cells)
+    var generationNumber = 0
     title = "Scala life"
+    val button = new Button {
+      text = "next"
+    }
+    val label = new Label {
+      text = generationNumber.toString()
+    }
+    listenTo(button)
+    val gui = new LifeGUI(10,10, g)
     contents = new BoxPanel(Orientation.Vertical) {
-      val gui = new LifeGUI(10,10, g)
-      contents += gui
-      val button = new Button {
-        text = "next"
-      }
       contents += button
+      contents += label
+      contents += gui
+    }
+    reactions += {
+      case ButtonClicked(b) =>
+        generationNumber += 1
+        label.text = generationNumber.toString()
+        gui.g = gui.g.nextGeneration
+        gui.repaint()
     }
   }
 }
 
-class LifeGUI(val height: Int, val width: Int, val g: Generation) extends ScrollPane {
+class LifeGUI(val height: Int, val width: Int, var g: Generation) extends ScrollPane {
   val table = new Table(height, width) {
     rowHeight = 10
     
